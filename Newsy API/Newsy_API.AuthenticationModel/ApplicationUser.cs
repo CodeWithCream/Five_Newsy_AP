72b345ad59c3025395143f707f4b9df886ff00a0
Andrea Knez Karačić <zelenokosazelena@gmail.com>
2022-06-05 11:14:04 +0200

    feat: add AuthenticationModel
    User and ApplicationUser connected

---
+++ Newsy API/Newsy_API.AuthenticationModel/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Newsy_API.Model;
+
+namespace Newsy_API.AuthenticationModel
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public long UserRefId { get; set; }
+        public User User { get; set; }
+        public bool IsActivated { get; set; } = true;
+
+        public ApplicationUser(User user)
+        {
+            User = user;
+        }
+    }
+}
